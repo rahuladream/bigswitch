4eedd8c9a069e2ebb2ad678afdbe44b1b91fe8fb
Rahul Singh <rahul.singh4800@gmail.com>
2018-07-11 19:18:45 +0530

    Added stylesheet

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,12 +1,5 @@
 class RegistrationController < ApplicationController
 
 	def create
-		@user = User.new(user_params)
-
-		if @user.save
-			format.html { data: 'Comment was successfully created.' }
-			format.js   { }
-        	format.json { data: 'registered' }
-        else
-        	format.json { data: 'Error Found' }
+	end
 end
